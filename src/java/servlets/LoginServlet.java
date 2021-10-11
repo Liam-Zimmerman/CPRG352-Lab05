65859ccedbf03fd609e13a6b9df9cdd3b68b402c
854292 <854292@SAIT227683IT.ACDM.DS.SAIT.CA>
2021-10-11 01:44:39 -0600

    LoginServlet Functionality Created parts of the doPost method in LoginServlet.

--- src/java/servlets/LoginServlet.java
+++ src/java/servlets/LoginServlet.java
@@ -7,6 +7,9 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import models.User;
+import service.AccountService;
 
 
 public class LoginServlet extends HttpServlet {
@@ -15,7 +18,7 @@ public class LoginServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         
-         getServletContext().getRequestDispatcher("/WEB-INF/viewnote.jsp").forward(request, response);
+         getServletContext().getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
          return;
     }
 
@@ -24,6 +27,34 @@ public class LoginServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
        
+        String loginUsername = request.getParameter("username");
+        String loginPassword = request.getParameter("password");
+        
+        request.setAttribute("inputUsername", loginUsername);
+        request.setAttribute("inputPassword", loginPassword);
+        
+        if(loginUsername.equals("") || loginPassword.equals(""))
+        {
+            request.setAttribute("message", "Please enter a username and password.");
+            getServletContext().getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
+            return;
+        }
+        
+        AccountService service = new AccountService();
+        
+        User user = service.login(loginUsername, loginPassword);
+        
+        if(user == (null))
+        {
+            request.setAttribute("message", "Username or password are incorrect, please re-enter information.");
+            getServletContext().getRequestDispatcher("/WEB-INF/login.jsp").forward(request, response);
+            return;
+        }
+        
+        HttpSession session = request.getSession();
+        
+        session.setAttribute("sessionUsername", user.getUsername());
+        
+        response.sendRedirect("/MyLogin/home");     
     }
-    
 }
